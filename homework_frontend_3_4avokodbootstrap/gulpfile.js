9c0eb3d4ff754c0b2622c06735191fce5b40675b
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-10-04 15:25:04 +0300

    using gulp modules

--- homework_frontend_3_4avokodbootstrap/gulpfile.js
+++ homework_frontend_3_4avokodbootstrap/gulpfile.js
@@ -1,13 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    minifyCSS = require('gulp-minify-css');
 
-gulp.task('html', function() {
-  return gulp.src('client/templates/*.pug')
-  .pipe(gulp.dest('build/html'))
-});
-
-gulp.task('css', function() {
-  return gulp.src('client/templates/*.less')
+gulp.task('minify-css', function() {
+  return gulp.src('style.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(gulp.dest('build/css'))
 });
 
-gulp.task('default', ['html', 'css']);
+gulp.task('default', function() {
+  return gulp.src('images/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('build/images'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('style.css')
+  .pipe(minifyCSS({keepBreaks: true}))
+  .pipe(gulp.dest('build/css'))
+});
