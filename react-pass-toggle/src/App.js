8cdb8ea6cccded9e901b951fa28a6151b3db5751
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-03 16:09:51 +0300

    material-ui password toggle

--- react-pass-toggle/src/App.js
+++ react-pass-toggle/src/App.js
@@ -1,13 +1,84 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core';
+import { InputAdornment, withStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { RemoveRedEye } from '@material-ui/icons';
+
+const styles = theme => ({
+  eye: {
+    cursor: 'pointer'
+  }
+});
+
+class PasswordInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      passwordIsMasked: true
+    }
+  }
+
+  togglePasswordMask = () => {
+    this.setState(prevState => ({
+      passwordIsMasked: !prevState.passwordIsMasked
+    }))
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { passwordIsMasked } = this.state;
+
+    return <TextField 
+      type={passwordIsMasked ? "password" : 'text'} 
+      {...this.props}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <RemoveRedEye 
+              className={classes.eye}
+              onClick={this.togglePasswordMask}
+            />
+          </InputAdornment>
+        )
+      }}
+    />;
+  }
+}
+
+PasswordInput.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.func.isRequired
+};
+
+PasswordInput = withStyles(styles)(PasswordInput);
 
 class App extends Component {
-  render() {
-    return (
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      password: ''
+    };
+  }
+
+  onChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { password } = this.state;
+
+    return (
+      <PasswordInput
+        label="Password"
+        name="password"
+        value={password}
+        onChange={this.onChange}
+      />
     );
   }
 }
