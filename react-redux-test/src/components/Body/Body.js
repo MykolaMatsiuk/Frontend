0938a38d02b88b1887752e2ba19179a0c28dec48
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-05 20:28:43 +0300

    header to variable

--- react-redux-test/src/components/Body/Body.js
+++ react-redux-test/src/components/Body/Body.js
@@ -3,12 +3,16 @@ import logo from '../../logo.svg';
 
 class Body extends Component {
   render() {
+    const bodyHeader = (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        {bodyHeader}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
