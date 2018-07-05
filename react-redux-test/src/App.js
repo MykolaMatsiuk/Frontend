ab932818a0cb47b5c570ea5463dbd2add16ec58b
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-05 21:04:07 +0300

    preloader

--- react-redux-test/src/App.js
+++ react-redux-test/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
 import Feed from './components/Feed/Feed';
+import Preloader from './components/Preloader/Preloader';
 
 class App extends Component {
   constructor(props) {
@@ -14,15 +15,23 @@ class App extends Component {
   componentWillMount() {
     console.log('componentWillMount');
 
+    setTimeout(this.fetchDataFromServer, 3000);
+  }
+
+  fetchDataFromServer = () => {
     fetch('/feed.json')
       .then(res => res.json())
       .then(res => this.setState({
         news: res
-      }))
+      }));
   }
 
   render() {
     const age = 30;
+
+    if (!this.state.news) {
+      return <Preloader />
+    }
 
     return (
       <Fragment>
---
+++ react-redux-test/src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Preloader extends Component {
+  render() {
+    return (
+      <h1>Loading ...</h1>
+    );
+  }
+}
+
+export default Preloader;
