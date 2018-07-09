4b223cb271632bda702fca77b601f6d8bf68962c
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-09 10:31:48 +0300

    index.js calc funcs created and exported

---
+++ calc/index.js
@@ -0,0 +1,27 @@
+const calculator = () => {
+  let total,
+      firstN,
+      secondN;
+
+  return {
+    add: (firstN, secondN) => {
+      total = firstN + secondN;
+      return total;
+    },
+    substract: (firstN, secondN) => {
+      total = firstN - secondN;
+      return total;
+    },
+    divide: (firstN, secondN) => {
+      total = firstN / secondN;
+      return total;
+    },
+    multiply: (firstN, secondN) => {
+      total = firstN * secondN;
+      return total;
+    },
+    total: () => total;
+  }
+}
+
+module.exports = calculator();
