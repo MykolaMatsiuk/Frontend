35bfeba58fad46e6a5c3fef296df1052278f0b54
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-16 16:12:59 +0300

    making FormComponent functional

--- react-redux-example/src/form/FormComponent.js
+++ react-redux-example/src/form/FormComponent.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
 
-class FormComponent extends Component {
-  render() {
-    return (
-      <form onSubmit={this.props.onFormSubmit}>
-        <h1>Our form example</h1>
-        <div>
-          <textarea onChange={this.props.onTextChange}>
-            {this.props.text}
-          </textarea>
-        </div>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    );
-  }
-}
+const FormComponent = props => (
+  <form onSubmit={props.onFormSubmit}>
+    <h1>Our form example</h1>
+    <div>
+      <textarea onChange={props.onTextChange}>
+        {props.text}
+      </textarea>
+    </div>
+    <div>
+      <input type="submit" value="Submit" />
+    </div>
+  </form>
+);
 
 export default FormComponent;
