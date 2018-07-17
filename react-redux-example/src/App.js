9f201ac8349bd3bd510afbdb914cae132a0af1e0
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2018-07-17 14:15:54 +0300

    store, reducer, actions

--- react-redux-example/src/App.js
+++ react-redux-example/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./form/Form";
+import Form from "./Form/Form";
 
 class App extends Component {
   render() {
---
+++ react-redux-example/src/reducers.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import form from "./Form/reducer";
+
+export default combineReducers({ form });
